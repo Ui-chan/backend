be70cc3221d7d54880e7f5970d91429ab3caa187
an <dmlcks0209@naver.com>
2025-07-05 18:33:50 +0900

    feat: DB 연결, users 테이블

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    age = models.IntegerField(null=True, blank=True)
+    point = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users'  # 기존 테이블명과 맞추기
