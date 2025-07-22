ad68d770993ac6ab131c2f8799d02f890eb25471
an <dmlcks0209@naver.com>
2025-07-22 16:10:32 +0900

    feat: 회원가입 시 기본 캐릭터, 배경 설정

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import User
 
 class UserSignupSerializer(serializers.ModelSerializer):
-    # age 필드를 명시적으로 정의하여 필수로 만듦
     age = serializers.IntegerField(required=True, error_messages={
         'required': 'Age is required.',
         'invalid': 'Age must be a valid number.'
@@ -24,7 +23,18 @@ class UserSignupSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
+        # 자동으로 기본 캐릭터 및 배경 값 세팅
+        validated_data['base_character_name'] = 'cat'
+        validated_data['base_character_img'] = 'https://iccas-zerodose.s3.ap-northeast-2.amazonaws.com/character/cat/cat_main.png'
+        validated_data['base_background_name'] = 'yard'
+        validated_data['base_background_img'] = [
+            "https://iccas-zerodose.s3.ap-northeast-2.amazonaws.com/background/yard/set/yard1.jpg",
+            "https://iccas-zerodose.s3.ap-northeast-2.amazonaws.com/background/yard/set/yard2.png",
+            "https://iccas-zerodose.s3.ap-northeast-2.amazonaws.com/background/yard/set/yard3.jpg"
+        ]
+
         return User.objects.create(**validated_data)
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
