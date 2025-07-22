d111df9086ab9b81aaab79e425e91b51391dc217
an <dmlcks0209@naver.com>
2025-07-23 01:36:06 +0900

    feat: 1단계 퀴즈 저장, 아이템 받아오기 API

--- item/urls.py
+++ item/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # item/
     path('all/', ItemListView.as_view()),
     path('buy/', AddItemToUserView.as_view()),
     path('base-setting/', UpdateBaseView.as_view()),
+    path('userid-to-emotions/', BaseCharacterDetailView.as_view()),
 ]
